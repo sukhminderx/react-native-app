b6267230a7b3724aeefcfb7dc3ca6a4489d3f32e
gabruism <sukhminderxliterature@gmail.com>
2022-07-24 00:16:07 +0530

    drawer navigator added to navigations

--- src/navigations/DrawerNavigator.tsx
+++ src/navigations/DrawerNavigator.tsx
@@ -7,7 +7,7 @@ const DrawerNavigator = () => {
     const Drawer = createDrawerNavigator();
 
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator >
     <Drawer.Screen name={HOME_NAVIGATOR} component={HomeNavigator} />
   </Drawer.Navigator>
   );
--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -1,11 +1,13 @@
  import React from 'react';
  import { NavigationContainer } from '@react-navigation/native';
 import DrawerNavigator from './DrawerNavigator';
+import AuthNavigator from './AuthNavigator';
  
 const AppNavContainer = () => {
+  const isAuthenticated = true;
    return (
      <NavigationContainer>       
-         <DrawerNavigator />
+        {isAuthenticated ? <DrawerNavigator /> : <AuthNavigator />}
      </NavigationContainer>
    );
  };
