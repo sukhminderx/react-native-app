1a68eb29b741e3af92821567aa5e833d9716c187
gabruism <sukhminderxliterature@gmail.com>
2022-07-23 23:15:24 +0530

    added index navigation

---
+++ src/navigations/index.tsx
@@ -0,0 +1,20 @@
+ import React from 'react';
+ import { NavigationContainer } from '@react-navigation/native';
+ import {  
+   Text
+ } from 'react-native';
+ 
+const AppNavContainer = () => {
+   return (
+     <NavigationContainer>       
+         <Text>Hello</Text>
+     </NavigationContainer>
+   );
+ };
+ 
+ export default AppNavContainer;
+ 
+ /*
+  screens > home > drawer
+  screens> auth
+ */
